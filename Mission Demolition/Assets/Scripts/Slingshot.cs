1baba21eed97bfee3fe5bf53f649d24806c36966
thompsonpat <thompsonpat21@gmail.com>
2018-10-22 23:27:15 -0400

    slingshot halo hover

---
+++ Mission Demolition/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+	public GameObject launchPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Awake() {
+		Transform launchPointTrans = transform.Find("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive(false);
+	}
+
+	void OnMouseEnter() {
+		// print("Slingshot:OnMouseEnter()");
+		launchPoint.SetActive(true);
+	}
+
+	void OnMouseExit() {
+		// print("Slingshot:OnMouseExit()");
+		launchPoint.SetActive(false);
+	}
+}
