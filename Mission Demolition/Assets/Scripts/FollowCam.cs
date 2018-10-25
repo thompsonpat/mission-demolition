43d7c5f8450a88049461b37147b1667cfe77bc08
thompsonpat <thompsonpat21@gmail.com>
2018-10-24 21:10:12 -0400

    Camera resets after projectile comes to rest

--- Mission Demolition/Assets/Scripts/FollowCam.cs
+++ Mission Demolition/Assets/Scripts/FollowCam.cs
@@ -8,9 +8,9 @@ public class FollowCam : MonoBehaviour
     // static point of interest
     static public GameObject POI;
 
-	[Header ("Set in Inspector")]
-	public float easing = 0.05f;
-	public Vector2 minXY = Vector2.zero;
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
 
     [Header("Set Dynamically")]
     public float camZ;
@@ -22,18 +22,40 @@ public class FollowCam : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (POI == null) return;
-		Vector3 destination = POI.transform.position;
-		// Limit the X & Y to minimum values
-		destination.x = Mathf.Max(minXY.x, destination.x);
-		destination.y = Mathf.Max(minXY.y, destination.y);
-		destination = Vector3.Lerp(transform.position, destination, easing);
-		// Keep camera away on Z axis
-		destination.z = camZ;
-		// Set the camera to the destination
-		transform.position = destination;
-		// Change orthographicSize to keep ground in view
-		Camera.main.orthographicSize = destination.y + 10;
+        Vector3 destination;
+
+        // If there is no POI, return to P:[0,0,0]
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            // Get the position of the POI
+            destination = POI.transform.position;
+            // If POI is a Projectile, check to see if it's at rest
+            if (POI.tag == "Projectile")
+            {
+                // If it is sleeping (not moving)
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    // Return to default view
+                    POI = null;
+                    // In the next update
+                    return;
+                }
+            }
+        }
+        // Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        // Keep camera away on Z axis
+        destination.z = camZ;
+        // Set the camera to the destination
+        transform.position = destination;
+        // Change orthographicSize to keep ground in view
+        Camera.main.orthographicSize = destination.y + 10;
     }
 
     // Use this for initialization
